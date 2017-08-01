20b7da4420784530729872eb99326ef1bda66e87
Lucia Vilallonga <laliferous@gmail.com>
2017-08-01 09:34:24 -0400

    cleaned up repo

--- Sentiment/sentiment_classifier.py
+++
@@ -1,9 +0,0 @@
-class Sentiment_Classifier:
-    """
-    Given a statement (text or speech), determine whether it is positive (+1) or negative (-1). Trained on movie review data collected by Cornell University.
-    """
-
-    def __init__(self):
-        pass
-
-    
