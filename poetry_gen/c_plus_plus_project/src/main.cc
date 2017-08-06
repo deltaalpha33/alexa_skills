c8122f5862fd3977fc9a350e767fa78a6d2fca10
deltaalpha33 <dalexakos@gmail.com>
2017-08-06 08:57:07 -0400

    started on lstm

---
+++ poetry_gen/c_plus_plus_project/src/main.cc
@@ -0,0 +1,15 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
+/*
+ * main.cc
+ * Copyright (C) 2017 Dimitris Alexakos <dalexakos1321@bths.edu>
+ * 
+ */
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello world!" << std::endl;
+	return 0;
+}
+
